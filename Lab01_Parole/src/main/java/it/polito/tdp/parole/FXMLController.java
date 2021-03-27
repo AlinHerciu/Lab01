3f5a27e89d4d5e63199c9ce0bd8749281d2c1213
utente <utente@LAPTOP-E2PEJK5Q.home-life.hub>
2021-03-27 13:13:47 +0100

    Soluzione lab 01

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -1,8 +1,11 @@
 package it.polito.tdp.parole;
 
+
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,9 +15,9 @@ import javafx.scene.control.TextField;
 
 public class FXMLController {
 	
-	Parole elenco ;
-
-    @FXML
+	Parole elenco; //creo fuori da qualsiasi metodo cosi questo è un oggetto persistente
+	
+	 @FXML
     private ResourceBundle resources;
 
     @FXML
@@ -28,18 +31,80 @@ public class FXMLController {
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    private TextArea txtPerformance;
 
     @FXML
     private Button btnReset;
-
+    
+    /**
+    * Cancella la parola selezionata
+    *   @param event
+    */
     @FXML
-    void doInsert(ActionEvent event) {
-    	// TODO
+    void doCancella(ActionEvent event) { //devo passare al metodo removeParola la parola selezionata dal''utente
+    	String selected = this.txtResult.getSelectedText(); //metodo nuovo
+    	
+    	double start= System.nanoTime();
+    	elenco.removeParola(selected);
+    	double stop= System.nanoTime();
+    	
+    	double start2= System.nanoTime();
+    	elenco.removeParola2(selected);
+    	double stop2= System.nanoTime();
+    	
+    	String result = "";
+    	for(String p: elenco.getElenco()) {
+      	  result += p +"\n";
+        }
+        
+    	txtResult.setText(result);
+    	
+    	this.txtPerformance.clear();
+    	txtPerformance.setText("Tempo di esecuzione operazione CANCELLA con ArrayList: "+ (stop - start)+" ms \n"+ 
+    	"Tempo di esecuzione operazione CANCELLA con LinkedList: "+ (stop2 - start2)+" ms");
     }
 
     @FXML
+    void doInsert(ActionEvent event) {
+      double start= System.nanoTime();
+      elenco.addParola(this.txtParola.getText()); //memorizza l'input dell'utente nella lista di elencoParole
+      double stop= System.nanoTime();
+      
+      double start2= System.nanoTime();
+      elenco.addParola2(this.txtParola.getText()); //memorizza l'input dell'utente nella lista di elencoParole
+      double stop2= System.nanoTime();
+      
+      txtResult.clear();
+      
+      String result = "";
+      for(String p: elenco.getElenco()) {
+    	  result += p +"\n";
+      }
+      
+      txtResult.setText(result);
+      
+      this.txtPerformance.clear();
+      this.txtPerformance.setText("Tempo di esecuzione operazione INSERISCI con ArrayList: "+ (stop - start)+" ms \n" +
+      "Tempo di esecuzione operazione INSERISCI con LinkedList: "+ (stop2 - start2)+" ms");
+      this.txtParola.clear();
+      }
+
+    @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	double start= System.nanoTime();
+    	elenco.reset();
+    	double stop= System.nanoTime();
+    	
+    	double start2= System.nanoTime();
+    	elenco.reset2();
+    	double stop2= System.nanoTime();
+    	this.txtResult.clear();
+    	
+        this.txtPerformance.clear();
+        this.txtPerformance.setText("Tempo di esecuzione operazione RESET con ArrayList : "+ (stop - start)+" ms \n"+
+        "Tempo di esecuzione operazione RESET con LinkedList : "+ (stop2 - start2)+" ms");
     }
 
     @FXML
@@ -48,7 +113,11 @@ public class FXMLController {
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-
-        elenco = new Parole() ;
+        assert txtPerformance != null : "fx:id=\"txtPerformance\" was not injected: check your FXML file 'Scene.fxml'.";
+     
+        elenco = new Parole();
     }
+
+	
+    
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,83 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
+
+
 public class Parole {
-		
-	public Parole() {
-		//TODO
+	
+	private List<String> elencoParole;  
+	private List<String> elencoWords;
+	
+	
+	public Parole() { 						
+		elencoParole = new ArrayList<String>();
+		elencoWords = new LinkedList<String>();
 	}
 	
+	/**
+	 * aggiunge la nuova parola all'elenco di parole
+	 * @param p
+	 */
 	public void addParola(String p) {
-		//TODO
+	 elencoParole.add(p); //aggiungo la parola passata come parametro alla lista di parole
+	 
 	}
 	
-	public List<String> getElenco() {
-		//TODO
-		return null;
-	}
+	public void addParola2(String p) {
+		 elencoWords.add(p);
+		}
 	
-	public void reset() {
-		// TODO
+	/**
+	 * Restituisce l'elenco delle parole inserite ordinate alfabeticamente
+	 * @return
+	 */
+	
+	public List<String> getElenco() { 
+		Collections.sort(elencoParole);
+		return elencoParole;
+		}
+	
+	
+	public void reset() { 
+		elencoParole.clear();
 	}
 
+	public void reset2() { 
+		elencoWords.clear();
+	}
+	
+	public List<String> getElencoWords(){
+		Collections.sort(elencoWords);
+		return elencoWords;
+	}
+	
+	public void setElencoWords(List<String> elencoWords) {
+		this.elencoWords = elencoWords;
+	}
+
+	public List<String> getElencoParole() {
+		return elencoParole;
+	}
+
+	public void setElencoParole(List<String> elencoParole) {
+		this.elencoParole = elencoParole;
+	}
+
+	
+	
+	/**
+	 * Rimuove dalla lista elencoParole la parola passata come parametro
+	 * @param string
+	 */
+	public void removeParola(String string) { 
+		elencoParole.remove(string);
+	}
+	
+	public void removeParola2(String string) { 
+		elencoWords.remove(string);
+	}
 }
